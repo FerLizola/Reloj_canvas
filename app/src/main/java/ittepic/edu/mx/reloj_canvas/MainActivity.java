427906118017e29d4d1b2d19d7278bed3a604100
NandoLizola <ferlizola95@gmail.com>
2017-11-08 05:03:51 -0700

    Initial commit

---
+++ app/src/main/java/ittepic/edu/mx/reloj_canvas/MainActivity.java
@@ -0,0 +1,20 @@
+package ittepic.edu.mx.reloj_canvas;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Reloj a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
